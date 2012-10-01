7f845a1c514026ab5e8cfb06210bbbc6bc8c4418
Jeremy Leipzig <leipzig@gmail.com>
2012-10-01 17:09:49 -0400

    checked

--- R/berrylogo.R
+++ R/berrylogo.R
@@ -1,14 +1,13 @@
-library(plyr)
-library(ggplot2)
-library(reshape2)
-
 ##' Generates a Berry logo in ggplot from a positional weight matrix
 ##'
 ##' @param pwm matrix of fractional nucleotide frequencies 0.00-1.00
-##'       here rows are A, C, G, T and columns are position
-##'       gc_content the GC content of the source genome    
-##'       zero  a placeholder estimate for true frequency when 0 is observed
+##'    here rows are A, C, G, T and columns are position
+##' @param gc_content optional the GC content of the source genome    
+##' @param zero optional a placeholder estimate for true frequency when 0 is observed
 ##' @return A ggplot2 object, use print() to display
+##' 
+##' @export
+##' 
 ##' @examples
 ##' freqs<-matrix(data=c(0.25,0.65,0.87,0.92,0.16,0.16,0.04,0.98,0.98,1.00,0.02,0.10,0.10,0.80,0.98,0.91,0.07,0.07,
 ##'    0.11,0.05,0.04,0.00,0.26,0.17,0.00,0.01,0.00,0.00,0.29,0.17,0.01,0.03,0.00,0.00,0.32,0.32,
@@ -19,13 +18,13 @@ library(reshape2)
 berrylogo<-function(pwm,gc_content=.41,zero=.0001){  
   backFreq<-list(A=(1-gc_content)/2,C=gc_content/2,G=gc_content/2,T=(1-gc_content)/2)
   pwm[pwm==0]<-zero
-  bval<-laply(names(backFreq),function(x){log(pwm[x,])-log(backFreq[[x]])})
+  bval<-plyr::laply(names(backFreq),function(x){log(pwm[x,])-log(backFreq[[x]])})
   row.names(bval)<-names(backFreq)
-  p<-ggplot(melt(bval,varnames=c("nt","pos")),aes(x=pos,y=value,label=nt))+
-    geom_abline(aes(slope=0), colour = "grey",size=2)+
-    geom_text(aes(colour=factor(nt)),size=8)+
-    theme(legend.position="none")+
-    scale_x_continuous(name="Position",breaks=1:ncol(bval))+
-    scale_y_continuous(name="Log relative frequency")
+  p<-ggplot2::ggplot(reshape2::melt(bval,varnames=c("nt","pos")),ggplot2::aes(x=pos,y=value,label=nt))+
+    ggplot2::geom_abline(ggplot2::aes(slope=0), colour = "grey",size=2)+
+    ggplot2::geom_text(ggplot2::aes(colour=factor(nt)),size=8)+
+    ggplot2::theme(legend.position="none")+
+    ggplot2::scale_x_continuous(name="Position",breaks=1:ncol(bval))+
+    ggplot2::scale_y_continuous(name="Log relative frequency")
   return(p)
 }
