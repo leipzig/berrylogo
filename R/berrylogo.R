a00a8d6c5c3a70c68fec385e9196a6c5666dd88b
Jeremy Leipzig <leipzig@gmail.com>
2012-10-01 16:40:46 -0400

    NAMESPACE

--- R/berrylogo.R
+++ R/berrylogo.R
@@ -4,19 +4,19 @@ library(reshape2)
 
 ##' Generates a Berry logo in ggplot from a positional weight matrix
 ##'
-##' @param pwm  matrix of fractional nucleotide frequencies 0.00-1.00
-##'             here rows are A, C, G, T and columns are position
+##' @param pwm matrix of fractional nucleotide frequencies 0.00-1.00
+##'       here rows are A, C, G, T and columns are position
 ##'       gc_content the GC content of the source genome    
 ##'       zero  a placeholder estimate for true frequency when 0 is observed
 ##' @return A ggplot2 object, use print() to display
-##' @example
+##' @examples
 ##' freqs<-matrix(data=c(0.25,0.65,0.87,0.92,0.16,0.16,0.04,0.98,0.98,1.00,0.02,0.10,0.10,0.80,0.98,0.91,0.07,0.07,
-##'  0.11,0.05,0.04,0.00,0.26,0.17,0.00,0.01,0.00,0.00,0.29,0.17,0.01,0.03,0.00,0.00,0.32,0.32,
+##'    0.11,0.05,0.04,0.00,0.26,0.17,0.00,0.01,0.00,0.00,0.29,0.17,0.01,0.03,0.00,0.00,0.32,0.32,
 ##'    0.53,0.26,0.07,0.02,0.53,0.18,0.96,0.01,0.00,0.00,0.65,0.01,0.89,0.17,0.01,0.09,0.59,0.12,
 ##'    0.11,0.04,0.02,0.06,0.05,0.49,0.00,0.00,0.02,0.00,0.04,0.72,0.00,0.00,0.01,0.00,0.02,0.49),byrow=TRUE,nrow=4,dimnames=list(c('A','C','G','T')))
-##' p<-berryLogo(freqs)
+##' p<-berrylogo(freqs)
 ##' print(p)
-berryLogo<-function(pwm,gc_content=.41,zero=.0001){  
+berrylogo<-function(pwm,gc_content=.41,zero=.0001){  
   backFreq<-list(A=(1-gc_content)/2,C=gc_content/2,G=gc_content/2,T=(1-gc_content)/2)
   pwm[pwm==0]<-zero
   bval<-laply(names(backFreq),function(x){log(pwm[x,])-log(backFreq[[x]])})
